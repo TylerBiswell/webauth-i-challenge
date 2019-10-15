3a86ea36697baeca521b980a2ed7404db2c27658
Tyler Biswell <tsbiswell@gmail.com>
2019-10-15 10:41:58 -0600

    added POST /api/register endpoint

--- users/users-router.js
+++ users/users-router.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bcrypt = require('bcryptjs');
 
 const Users = require('./users-model');
 
@@ -6,7 +7,26 @@ const router = express();
 
 // POST /api/register Endpoint
 router.post('/register', (req, res) => {
-  res.send('POST /api/register endpoint');
+  // res.send('POST /api/register endpoint');
+  let user = req.body;
+
+  if (user.username && user.password) {
+    const hash = bcrypt.hashSync(user.password, 8);
+
+    user.password = hash;
+
+    Users.add(user)
+      .then(saved => {
+        res.status(201).json(saved);
+      })
+      .catch(err => {
+        res.status(500).json(err);
+      });
+  } else {
+    res
+      .status(400)
+      .json({ message: 'Please provide registration information' });
+  }
 });
 
 // POST /api/login Endpoint
