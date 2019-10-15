37d723cb7067c1932de5efe8e08a31cc989e95ae
Tyler Biswell <tsbiswell@gmail.com>
2019-10-15 10:44:30 -0600

    MVP may finish stretch later

--- users/users-router.js
+++ users/users-router.js
@@ -3,6 +3,8 @@ const bcrypt = require('bcryptjs');
 
 const Users = require('./users-model');
 
+const protected = require('../auth/protected-middleware');
+
 const router = express();
 
 // POST /api/register Endpoint
@@ -53,8 +55,30 @@ router.post('/login', (req, res) => {
 });
 
 // GET /api/users Endpoint
-router.get('/users', (req, res) => {
-  res.send('GET /api/users endpoint');
+router.get('/users', protected, (req, res) => {
+  // res.send('GET /api/users endpoint');
+  Users.find()
+    .then(users => {
+      res.json(users);
+    })
+    .catch(err => res.send(err));
 });
+
+// function protected(req, res, next) {
+//   const { username, password } = req.headers;
+
+//   Users.findBy({ username })
+//     .first()
+//     .then(user => {
+//       if (user && bcrypt.compareSync(password, user.password)) {
+//         next();
+//       } else {
+//         res.status(401).json({ message: 'You shall not pass!' });
+//       }
+//     })
+//     .catch(err => {
+//       res.status(500).json(err);
+//     });
+// }
 
 module.exports = router;
